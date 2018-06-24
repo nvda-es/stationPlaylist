541dde423bd2f4d2b77daa92e0c09e0d5ef7a595
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-24 09:30:50 -0700

    LTS18: let the prep phase begin.

--- addon/appModules/splstudio/splupdate.py
+++ addon/appModules/splstudio/splupdate.py
@@ -76,7 +76,7 @@ _updatePickle = os.path.join(globalVars.appArgs.configPath, "splupdate.pickle")
 channels={
 	"stable":"https://addons.nvda-project.org/files/get.php?file=spl",
 	"try":"http://www.josephsl.net/files/nvdaaddons/getupdate.php?file=spl-try",
-	#"lts":"http://www.josephsl.net/files/nvdaaddons/getupdate.php?file=spl-ltsxp",
+	"lts":"http://www.josephsl.net/files/nvdaaddons/getupdate.php?file=spl-lts18prep",
 }
 
 # Available channels and their UI labels (if there's only one, channel selection list will not be shown).
@@ -84,6 +84,7 @@ channels={
 _updateChannels = [
 	("try","Test Drive Fast"),
 	("dev","Test Drive Slow"),
+	("lts","Test Drive LTS Prep"),
 	("stable","stable")
 ]
 
